744235b92c88c84d0c47c0862cb75e947e7eb105
G Reshma Reddy <saikrishna.sangu1@gmail.com>
2024-11-26 11:43:54 +0530

    first commit

---
+++ src/main/java/com/example/reacts/SecondspringappforreactvirtusaApplication.java
@@ -0,0 +1,13 @@
+package com.example.reacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondspringappforreactvirtusaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondspringappforreactvirtusaApplication.class, args);
+	}
+
+}
